9ba3f9a609bc9fdfd763143e0d5b58b42ae14a4d
caglayan70 <65968577+caglayan70@users.noreply.github.com>
2022-10-23 21:43:40 +0300

    Update homework10.sql

--- homework10.sql
+++ homework10.sql
@@ -8,4 +8,4 @@ RIGHT JOIN customer ON customer.customer_id = payment.customer_id;
 
 -- 3)customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz FULL JOIN sorgusunu yazınız.
 SELECT rental_id,first_name,last_name FROM customer
-Full JOIN rental ON rental.customer_id = customer.customer_id;
+FULL JOIN rental ON rental.customer_id = customer.customer_id;
